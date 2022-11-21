dede7d01ea05d41d859d7491ff8b03b236eaa95e
Jeison Stiven Lenis <98065779+Jeison0710@users.noreply.github.com>
2022-11-21 16:49:46 -0500

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 pwd
 cd code
-export OMP_NUM_THREADS=20
-gfortran runer.f90 -o parallel -fopenmp 
-time ./parallel
+#export OMP_NUM_THREADS=42
+
+
+	gfortran -c nrtype.f90
+	gfortran -c modules.f90
+	gfortran -c gaussint.f
+	gfortran -c evaluate_bsp.f90 
+	gfortran -c evaluate_bsp_v2.f90 
+	gfortran -c bspline_functions.f90
+	gfortran -c matrices_pbc.f90
+	gfortran -c initial_wavepacketpbc.f90
+	gfortran -c backwardsubstitution-delande.f90
+	gfortran -c llt-delande.f90
+	gfortran -c subr_scalefactor.f90
+	gfortran -c subr2_scalefactor.f90
+#	gfortran -o tt schrodinger_poisson_bsplines.f90 modules.o nrtype.o gaussint.o evaluate_bsp_v2.o bspline_functions.o initial_wavepacketpbc.o matrices_pbc.o llt-delande.o backwardsubstitution-delande.o subr_scalefactor.o -lblas -llapack
+	gfortran -o tt schrodinger_poisson_bsplines.f90 modules.o nrtype.o gaussint.o evaluate_bsp_v2.o bspline_functions.o initial_wavepacketpbc.o matrices_pbc.o llt-delande.o backwardsubstitution-delande.o subr2_scalefactor.o -lblas -llapack
+
+	./tt < inschrodinger_poisson_bsplines.dat
+	rm *.o *.mod
